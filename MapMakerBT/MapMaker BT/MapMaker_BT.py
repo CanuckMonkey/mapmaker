c966538e4dc59fee2f146420a468a78ef5375697
Donald Jackson <donald@jacksonfamily.ca>
2015-11-19 19:19:51 -0700

    Set up initial docstring & imports Define basic classes MapSpace and Map

--- MapMakerBT/MapMaker BT/MapMaker_BT.py
+++ MapMakerBT/MapMaker BT/MapMaker_BT.py
@@ -1,1 +1,41 @@
 #!/usr/bin/env python
+
+""" Map Creation Tool
+
+    This tool is designed with an initial focus on Bard's Tale-style maps.
+
+    Maps are built on a cartesian grid.  Each cell in the grid defines its own contents as well as the contents of the four bounding walls.
+
+"""
+
+try:
+    import os
+    import sys
+    import pygame
+except ImportError, err:
+    print "Could not load module: %s" % (err)
+    sys.exit(2)
+
+if not pygame.font: print "Warning: fonts disabled."
+
+class MapSpace():
+    """Define a single cell of the map."""
+
+    def __init__(self, coords=(0, 0)):
+        self.coords = coords
+        self.darkness = False
+        self.spinner = False
+        self.antiMagic = False
+        self.odd = False
+        self.special = False
+        self.encounter = False
+
+class Map():
+    """Define one complete map."""
+
+    def __init__(self, width=22, height=22):
+        self.width = width
+        self.height = height
+        for x in range(width):
+            for y in range(height):
+                self.map[x][y] = MapSpace(coords=(x, y))
