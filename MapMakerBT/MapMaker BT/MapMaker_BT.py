79926379b81c7e8ee03add0ddbd2727703ab0be7
Donald Jackson <donald@jacksonfamily.ca>
2015-11-20 10:52:48 -0700

    Add main function Add Options class

--- MapMakerBT/MapMaker BT/MapMaker_BT.py
+++ MapMakerBT/MapMaker BT/MapMaker_BT.py
@@ -1,22 +1,38 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
 """ Map Creation Tool
 
-    This tool is designed with an initial focus on Bard's Tale-style maps.
+This tool is designed with an initial focus on Bard's Tale-style maps.
 
-    Maps are built on a cartesian grid.  Each cell in the grid defines its own contents as well as the contents of the four bounding walls.
-
+Maps are built on a cartesian grid.  Each cell in the grid defines its own contents as well as the contents of the four bounding walls.
 """
+
+# A lot of base code copied from my earlier SoC project.  When I know something is needed but exact use is unclear, I have used ??? as a searchable string for later lookup.
 
 try:
     import os
     import sys
     import pygame
-except ImportError, err:
+    #from pygame.locals import *     # Bad form, should import only needed items for clarity
+except ImportError as err:
     print "Could not load module: %s" % (err)
     sys.exit(2)
 
 if not pygame.font: print "Warning: fonts disabled."
+
+def main():
+    pygame.init()
+
+    while True:     # Loop while program is running
+        clock.tick(60)      # Where does clock come from and what does this do???
+
+class Options():
+    """Define the options in use for this map."""
+
+    def __init__(self):
+        self.wraparound = True
+        self.coordDisplay = True
+        
 
 class MapSpace():
     """Define a single cell of the map."""
@@ -39,3 +55,6 @@ class Map():
         for x in range(width):
             for y in range(height):
                 self.map[x][y] = MapSpace(coords=(x, y))
+
+if __name__ == "__main__":      # Do not run this if called from some other module, I think???
+    main()
